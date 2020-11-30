680e48bb8c254d8f5c24e099af756ae6a181be5d
Mcleod <marshmcl@3c22fb7f50ce.ant.amazon.com>
2020-11-30 14:25:19 -0500

    Added ps1 script to bckup db locally & to S3Bucket. Also deletes artifacts locally

---
+++ sqltos3bckp.ps1
@@ -0,0 +1,30 @@
+Set-AWSCredentials myAWSCredentials
+
+
+$server = '.'
+$backupPath = 
+$s3Bucket = ''
+$region = 'us-east-1'
+
+$databases = Invoke-Sqlcmd -ServerInstance $server -Query "SELECT [name] FROM master.dbo.sysdatabases where [name] = 'MyNewDB'"
+
+foreach ($database in $databases)
+{
+    $timestamp = get-date -Format MMddyyyyHHmmss
+    $fileName = "$($database.name) -$timestamp.bak"
+    $zipfileName = "$($database.name) -$timestamp.zip"
+    $filePath = Join-Path $backupPath $fileName
+    $zipfilepath = Join-Path $backupPath $zipfileName
+}
+
+Backup-SqlDatabase -ServerInstance $server -Database $database.name -BackupFile $filePath -CompressionOption On -checksum
+
+Write-Zip -path $filePath -OutputPath $zipfilepath 
+
+Write-S3Object -BucketName $s3Bucket -StoreCredentials -File $zipfilepath $zipfileName -Region $region
+
+
+Remove-Item $backupPath$($database.name)*.bak
+Remove-Item $backupPath$($database.name)*.zip
+
+
