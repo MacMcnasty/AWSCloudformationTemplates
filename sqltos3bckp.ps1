4c1dde0cc33e6626fe066da4822937318da3aacd
Mcleod <marshmcl@3c22fb7f50ce.ant.amazon.com>
2020-12-02 15:29:24 -0500

    Updated all projects with new code

--- sqltos3bckp.ps1
+++ sqltos3bckp.ps1
@@ -1,10 +1,11 @@
-Set-AWSCredentials myAWSCredentials
+#Set-AWSCredentials myAWSCredentials
 
 
 $server = '.'
 $backupPath = 
 $s3Bucket = ''
-$region = 'us-east-1'
+#$region = 'us-east-1'
+$rention = 
 
 $databases = Invoke-Sqlcmd -ServerInstance $server -Query "SELECT [name] FROM master.dbo.sysdatabases where [name] = 'MyNewDB'"
 
@@ -15,13 +16,14 @@ foreach ($database in $databases)
     $zipfileName = "$($database.name) -$timestamp.zip"
     $filePath = Join-Path $backupPath $fileName
     $zipfilepath = Join-Path $backupPath $zipfileName
+
 }
 
 Backup-SqlDatabase -ServerInstance $server -Database $database.name -BackupFile $filePath -CompressionOption On -checksum
 
 Write-Zip -path $filePath -OutputPath $zipfilepath 
 
-Write-S3Object -BucketName $s3Bucket -StoreCredentials -File $zipfilepath $zipfileName -Region $region
+Write-S3Object -BucketName $s3Bucket -File $zipfilepath $zipfileName -Region $region
 
 
 Remove-Item $backupPath$($database.name)*.bak
